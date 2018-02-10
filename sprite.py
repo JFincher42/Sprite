61c3ad8b78901ccd22380b781380961eb886b212
Jon Fincher <jfincher42@gmail.com>
2018-02-09 16:19:43 -0800

    Updated test to move sprite, refactored some stuff in the class

--- sprite.py
+++ sprite.py
@@ -8,18 +8,25 @@ BLACK = (0,   0,   0)
 class Sprite(pygame.sprite.Sprite):
 
     def __init__(self, image, xpos, ypos):
+        # Call the super init first
         pygame.sprite.Sprite.__init__(self)
+
+        # Load the image we're given,set transparency, and get the bounding rectangle
         self.image = pygame.image.load(image).convert_alpha()
         self.rect = self.image.get_rect()
+        self.image.set_colorkey(WHITE)
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.center = (xpos+self.width/2, ypos+self.height/2)
+
+        # Set the position of the item
         self.rect.x = xpos
         self.rect.y = ypos
-        self.image.set_colorkey(WHITE)
         self.position = (xpos, ypos)
-        width = self.image.get_width()
-        height = self.image.get_height()
-        self.center = (xpos+width/2, ypos+height/2)
 
+        # Add this to it's own group for rendering
         self.mygroup = pygame.sprite.GroupSingle(self)
 
+    # Draws the sprite onto the provided surface
     def draw(self, surf):
         self.mygroup.draw(surf)
--- spritetest.py
+++ spritetest.py
@@ -4,8 +4,11 @@ import os
 
 pygame.init()
 w = pygame.display.set_mode([400, 400])
+c = pygame.time.Clock()
 
-spaceship = sprite.Sprite(os.path.join(".", "spaceship_sprite.png"), 100, 100)
+xpos=100
+ypos=100
+spaceship = sprite.Sprite(os.path.join(".", "spaceship_sprite.png"), xpos, ypos)
 
 drawing = True
 while drawing:
@@ -13,5 +16,11 @@ while drawing:
         if event.type == pygame.QUIT:
             drawing = False
 
+    w.fill((128,128,128))
+    xpos+=2
+    if xpos>400:
+        xpos=0
+    spaceship.rect.x = xpos
     spaceship.draw(w)
     pygame.display.flip()
+    c.tick(30)
